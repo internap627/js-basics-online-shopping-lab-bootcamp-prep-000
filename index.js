de014287ca0190a5a9e8bc2acd216efb75d199cc
Nkosi Phillip <internap627@gmail.com>
2018-09-06 04:49:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ return total;
 function removeFromCart(item) {
   // write your code here
   for(var i = 0 ; i < cart.length ; i++){
-    if(cart[i].itemName == name){
+    if(cart[i].itemName == item){
       cart.splice(i,1);
       return cart;
     }
